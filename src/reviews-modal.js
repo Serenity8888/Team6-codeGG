ef97d00d6471c293e4facda22b9a1bc84ce869ba
NicholaiNora <nicholai.nora.dict@gmail.com>
2024-02-03 10:48:06 +0800

    Subscribe Section

--- src/reviews-modal.js
+++ src/reviews-modal.js
@@ -1,8 +1,8 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
+    openModalBtn: document.querySelector('[reviews-data-modal-open]'),
+    closeModalBtn: document.querySelector('[reviews-data-modal-close]'),
+    modal: document.querySelector('[reviews-data-modal]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
